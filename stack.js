b039a5de41dc1dd41bbfe5808b26626f490fc115
nemostatus <mattcorralejo1@gmail.com>
2021-03-24 00:13:36 -0700

    created pop function

--- stack.js
+++ stack.js
@@ -9,9 +9,21 @@ push(element){
     this.count+=1
     return this.count - 1
 }
+
+pop(){
+    if(this.count ===0) return undefined
+   let deleteItem = this.items[this.count-1]
+   this.count -=1
+   console.log(`${deleteItem} removed`)
+   return deleteItem
+}
 }
 
 const stack = new Stack()
 stack.push(100)
 stack.push(200)
 stack.push(300)
+stack.pop()
+stack.pop()
+stack.pop()
+console.log(stack.pop())
