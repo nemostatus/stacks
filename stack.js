6b3da743d1b073b5280abe904986945278ea402f
nemostatus <mattcorralejo1@gmail.com>
2021-03-24 00:06:16 -0700

    creating a stack and push method

---
+++ stack.js
@@ -0,0 +1,17 @@
+class Stack{
+constructor(){
+    this.items = []
+    this.count = 0
+}
+push(element){
+    this.items[this.count] = element
+    console.log(`${element} added to ${this.count}`)
+    this.count+=1
+    return this.count - 1
+}
+}
+
+const stack = new Stack()
+stack.push(100)
+stack.push(200)
+stack.push(300)
